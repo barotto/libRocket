696576d315539873c1050c2e98b1f2b1649a049b
Marco Bortolin <barotto@gmail.com>
2016-11-06 15:41:15 +0100

    Revert to old behaviour for DecoratorTiled's background color
    Draw the background image with a white base color, which is the same
    behaviour as CSS. The proper way to specify an overlay color for the
    background would be to implement CSS3 multiple backgrounds.

--- Source/Core/DecoratorTiled.cpp
+++ Source/Core/DecoratorTiled.cpp
@@ -108,10 +108,7 @@ Vector2f DecoratorTiled::Tile::GetDimensions(Element* element)
 void DecoratorTiled::Tile::GenerateGeometry(std::vector< Vertex >& vertices, std::vector< int >& indices, Element* element, const Vector2f& surface_origin, const Vector2f& surface_dimensions, const Vector2f& tile_dimensions) const
 {
 	RenderInterface* render_interface = element->GetRenderInterface();
-	const Property* element_colour = element->GetProperty(BACKGROUND_COLOR);
 	Colourb quad_colour = Colourb(255, 255, 255);
-	if (element_colour)
-		quad_colour = element_colour->Get<Colourb>();
 
 	TileDataMap::iterator data_iterator = data.find(render_interface);
 	if (data_iterator == data.end())
